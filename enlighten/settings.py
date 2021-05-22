5596ad6dedd1e2d1e924d7a6d80dc8f7a12c78e3
Roshna Dhakhwa <dhakhwaros073@gmail.com>
2021-05-22 21:00:13 +0545

    Update settings.py

--- enlighten/settings.py
+++ enlighten/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%4e!1!9_jshop0h^405q!ysr)f=t2x)6!(58g5sbvh^i_6ufmw'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
